820546f60642a5e8de5432efa4f53235cccc6e96
Daniel Moore <sttyerase@gmail.com>
2020-05-14 18:44:26 -0400

    Added method to accept request parameters.

--- src/main/java/com/dbmi/demos/PICalcApplication.java
+++ src/main/java/com/dbmi/demos/PICalcApplication.java
@@ -3,6 +3,8 @@ package com.dbmi.demos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dbmi.money.Calculation;
@@ -14,14 +16,20 @@ public class PICalcApplication {
     private double principal    = 25000.00;
     private double interestRate = 3.85;
 
-	@GetMapping("/calculate")
+	@GetMapping("/reference")
 	public String home() {
 	    System.out.format("Calculating: months %d , principal $%,3.2f, interestRate  %3.2f%% \n", months, principal, interestRate);
 		return new Calculation().getCalculation(months, principal, interestRate);
 	} // HOME()
 
+    @GetMapping("/calculate/{months}/{principal}/{interestrate}")
+    public String calculate(@PathVariable("months") Integer months,@PathVariable("principal") Double principal, @PathVariable("interestrate") Double interestRate) {
+	    System.out.format("Calculating: months %d , principal $%,3.2f, interestRate  %3.2f%% \n", months, principal, interestRate);
+		return new Calculation().getCalculation(months, principal, interestRate);
+    } // GETCROPSBYID(LONG)
+
 	public static void main(String[] args) {
 		SpringApplication.run(PICalcApplication.class, args);
 	} // MAIN(STRING[])
 
-}
+} // CLASS
