c89ccd02c3d8c93a62a49530d3482a298fbf8a6a
Dan Moore <sttyerase@gmail.com>
2021-04-01 08:30:06 -0400

    Change return from String to ResponseEntity<String>.

--- src/main/java/com/dbmi/demos/PICalcApplication.java
+++ src/main/java/com/dbmi/demos/PICalcApplication.java
@@ -3,6 +3,7 @@ package com.dbmi.demos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,22 +15,27 @@ import com.dbmi.money.CalculationService;
 public class PICalcApplication {
 
 	@GetMapping("/reference")
-	public String reference() {  // REFERENCE METHOD FOR TESTING CONNECTIVITY
+	public ResponseEntity<String> reference() {  // REFERENCE METHOD FOR TESTING CONNECTIVITY
 		int months          = 48;
 		double principal    = 25000.00;
 		double interestRate = 3.85;
 	    System.out.format("Calculating: months %d , principal $%,3.2f, interestRate  %3.2f%% \n", months, principal, interestRate);
 		HttpHeaders myHeaders = new HttpHeaders();
 		myHeaders.set("Content-Type","application/json");
-		return new CalculationService().getCalculation(months, principal, interestRate);
+		return ResponseEntity.ok()
+				.headers(myHeaders)
+				.body(new CalculationService().getCalculation(months, principal, interestRate));
 	} // HOME()
 
     @GetMapping("/calculate/{months}/{principal}/{interestrate}")
-    public String calculate(@PathVariable("months") Integer months,@PathVariable("principal") Double principal, @PathVariable("interestrate") Double interestRate) {
+    public ResponseEntity<String> calculate(@PathVariable("months") Integer months,@PathVariable("principal") Double principal, @PathVariable("interestrate") Double interestRate) {
 	    System.out.format("Calculating: months %d , principal $%,3.2f, interestRate  %3.2f%% \n", months, principal, interestRate);
 		HttpHeaders myHeaders = new HttpHeaders();
 		myHeaders.set("content-type","application/json");
-		return new CalculationService().getCalculation(months, principal, interestRate);
+		myHeaders.set("Content-Type","application/json");
+		return ResponseEntity.ok()
+				.headers(myHeaders)
+				.body(new CalculationService().getCalculation(months, principal, interestRate));
     } // GETCROPSBYID(LONG)
 
 	public static void main(String[] args) {
